de65e3c6a40b20912a003ae88666d5538ecb5c4a
Zafer Aslan <zafer.aslan@ogr.dpu.edu.tr>
2021-02-07 14:04:56 +0300

    07.02.2021

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void MusteriEkle(Musteri musteri)
+        {
+            Console.WriteLine("Musteri eklendi: " + musteri.Ad +" "+ musteri.Soyad +" "+ musteri.Id+" "+musteri.Yas+" "+musteri.Cinsiyet+"\n--------\n");
+        }
+        public void MusteriListele(Musteri[] musteriler)
+        {
+            foreach (Musteri musteri in musteriler)
+            {
+                Console.WriteLine(musteri.Ad+" "+musteri.Soyad);
+            }
+            Console.WriteLine("\n---------\n");
+        }
+
+    }
+}
--- ClassMetotDemo/Program.cs
+++ ClassMetotDemo/Program.cs
@@ -6,7 +6,39 @@ namespace ClassMetotDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Zafer";
+            musteri1.Soyad = "Aslan";
+            musteri1.Id = 100;
+            musteri1.Yas = 21;
+            musteri1.Cinsiyet = 'E';
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Yiğitcan";
+            musteri2.Soyad = "Mergen";
+            musteri2.Id = 101;
+            musteri2.Yas = 28;
+            musteri2.Cinsiyet = 'E';
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Oktay";
+            musteri3.Soyad = "Yolcu";
+            musteri3.Id = 102;
+            musteri3.Yas =39 ;
+            musteri3.Cinsiyet = 'E';
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.MusteriEkle(musteri1);
+            musteriManager.MusteriEkle(musteri2);
+            musteriManager.MusteriEkle(musteri3);
+            musteriManager.MusteriListele(musteriler);
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
